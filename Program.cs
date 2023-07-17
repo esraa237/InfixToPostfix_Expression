0dde8a7032e95852bde54a2fb5df4d8223f6ce0f
esraa237 <esraafoda2000@gmail.com>
2023-07-17 04:29:19 +0300

    finish version 1

---
+++ Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace infix_to_postfix_v1
+{
+    public class Program
+    {
+        public bool IsOperator(char ch)
+        {
+            if(ch== '+' || ch == '-' || ch == '*' || ch == '/')
+            {
+                return true;
+            }
+            return false;
+        }
+        public int priority(char ch)
+        {
+            if (ch == '-' || ch == '+') return 1;
+            else if (ch == '*' || ch == '/') return 2;
+            else return 0;
+        }
+
+        public void convert(ref string infix,out string postfix)
+        {
+            postfix = "";
+            Stack<char> op_stack = new Stack<char>();
+            for (int i = 0; i < infix.Length; i++)
+            {
+                char ch = infix[i];
+                if (IsOperator(ch))
+                {
+                    if (op_stack.Count <= 0)
+                    {
+                        op_stack.Push(ch);
+                    }
+                    else
+                    {
+                        if (priority(ch) > priority(op_stack.Peek()))//example ch=* stack=+
+                        {
+                            op_stack.Push(ch);
+                        }
+                        else if(priority(ch) < priority(op_stack.Peek()))//example ch=+ stack=* 
+                        {
+                            postfix += op_stack.Pop();
+                            i--;
+                        }
+                        else//example ch=/ stack=* or ch=- stack=+
+                        {
+                            postfix += op_stack.Pop();
+                            op_stack.Push(ch);
+                        }
+                    }
+                }
+                else
+                {
+                    postfix += ch;
+                }
+            }
+            for (int j = 0; j <= op_stack.Count; j++)
+                postfix += op_stack.Pop();
+
+        } 
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the infix Expression: ");
+            string infix = Console.ReadLine();
+            string postfix = "";
+            Program p=new Program();
+            p.convert(ref infix,out postfix);
+            System.Console.WriteLine("InFix  :  " + infix);
+            System.Console.WriteLine("PostFix:  " + postfix);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ readme.md
@@ -0,0 +1,12 @@
+## Description
+I wanted to evaluate an mathimatical Expression so I searched to algorithms to do it 
+#### The first step that I made in this example:
+I converted infix to postfix
+Using the stack data structure is the best method for converting an infix expression to a postfix expression. It holds operators until both operands have been processed, and it reverses the order of operators in the postfix expression to match the order of operation.
+Postfix has a number of advantages over infix for expressing algebraic formulas. First, any formula can be expressed without parenthesis. Second, it is very convenient for evaluating formulas on computers with stacks. Third, infix operators have precedence.
+This is the first version that the infix expression whitout "()" priority
+I will make it and update the solution 
+## What I learned from solving this
+1. I learned about datastructure espasialy,stack.
+2. I learned about this algloritm.
+3. I learned that I can devided the main problem into small problems to solve it
